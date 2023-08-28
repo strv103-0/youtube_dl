a304ecbd01779e55dd3f7ff49dff6adfa0f42049
strv103 <112059527+strv103-0@users.noreply.github.com>
2023-08-28 14:42:38 +0900

    Update README.md

--- main.py
+++ main.py
@@ -18,16 +18,17 @@ import yt_dlp
 
 def download_playlist(url):
     ydl_opts = {
-        'format': 'bestaudio/best',
-        'outtmpl': 'downloads/%(title)s.%(ext)s',
-        'ffmpeg_location': 'C:/ffmpeg/bin/ffmpeg.exe',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
+        'format': 'bestaudio/best',                       # 영상화질 선택(화질을 선택하지 않으면 가장 좋은 화질로 다운로드)
+        'outtmpl': 'downloads/%(title)s.%(ext)s',         # 다운로드 경로 설정
+        'ffmpeg_location': 'C:/ffmpeg/bin/ffmpeg.exe',    # ffmpeg.exe 위치 지정
+        'ignoreerrors': True,                             # 오류 발생시 건너뛰기
+        'postprocessors': [{                              # 파일 변환
+            'key': 'FFmpegExtractAudio',                  # 변환 모듈 선택
+            'preferredcodec': 'mp3',                      # 파일 형식 설정
             }],
     }
     
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:               # 다운로드 시작
         ydl.download([url])
 
 # title 출력
