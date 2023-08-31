f0c6d539ca8eee52e38da640559d7f825a140736
strv103 <112059527+strv103-0@users.noreply.github.com>
2023-08-31 14:43:50 +0900

    2.2.1 Update

--- README.md
+++ README.md
@@ -3,11 +3,9 @@
 
 ## 사용법
 > 1. 파이썬 파일과 같은 위치에 **downloads**폴더를 추가한다.
-> 2. 터미널을 열고 `pip install yt-dlp`를 입력한다.
-> 3. [ffmpeg 설치 가이드](#ffmpeg-설치-가이드windows-버전)에 따라 ffmpeg를 설치한다.
-> 4. 파이썬 파일을 실행한다.
-> 5. 유튜브 **플레이리스트** URL을 입력한다.
-> 6. **downloads** 폴더에서 파일을 가져온다.
+> 3. 파이썬 파일을 실행한다.
+> 4. 유튜브 **플레이리스트** URL을 입력한다.
+> 5. **downloads** 폴더에서 파일을 가져온다.
 
 
 
@@ -15,31 +13,12 @@
 > - 1.0.0 플레이리스트 다운로드
 > - 1.1.0 실패한 다운로드, 변환 파일 목록 출력
 > - 1.1.1 path 버그 수정
-> - 1.2.1 라이브러리 자동설치
 > - 2.0.0 **pytube** 에서 **yt_dlp**로 라이브러리 변경
+> - 2.1.0 라이브러리 자동 설치
+> - 2.2.0 ffmpeg 설치 필요 X
+> - 2.2.1 에러 코드 추가
 
 
 
 ## 개발 중인 기능
 > - .py파일을 .exe로 제작
-> - ffmpeg 자동 설치
-> - 라이블러리 자동 설치
-
-
-
-## **ffmpeg** 설정 가이드(Windows 버전)
-### 다운로드 실패시 [**ffmpeg** 설치 가이드(공식 사이트)](#ffmpeg-설치-가이드공식-사이트)로 이동
-> 1. `ffmpeg.7z` 파일을 `C:/`에 압축을 해제한다.
-> 2. 시스템 환경 변수 편집에 들어가 **환경 변수** 버튼을 누르고 **Path**을 누르고 **편집**을 누른다.![image](https://github.com/strv103-0/youtube_dl/assets/112059527/83368f86-c7d9-484a-b450-81a6cdc72f3f)
-> 3. **새로 만들기**를 누른후 `C:\ffmpeg\bin`을 입력하고 저장하면 설정 끝이다.![image](https://github.com/strv103-0/youtube_dl/assets/112059527/83ca66aa-c5a2-4d7f-b054-d1d0531917c5)
-
-
-
-## **ffmpeg** 설치 가이드(공식 사이트)
-> 1. [**FFmpeg** 다운로드 페이지](https://ffmpeg.org/download.html)로 들어간다.
-> 2. **Windows** 이미지를 누른다. ![image](https://github.com/strv103-0/youtube_dl/assets/112059527/710c066e-eea6-4102-8e67-f4831e57ae49)
-> 3. `Windows builds from gyan.dev`버튼을 누른다.![image](https://github.com/strv103-0/youtube_dl/assets/112059527/293ad0ca-28ad-4ed4-9aa6-f2f94897276f)
-> 4. `ffmpeg-git-full.7z`를 눌러 파일을 다운로드 한다.![image](https://github.com/strv103-0/youtube_dl/assets/112059527/e56046fa-ce7a-4e40-bf76-6c0cf74bcc97)
-> 5. 다운로드 후 파일을 `C:/`에 압축을 해제한다.
-> 6. 시스템 환경 변수 편집에 들어가 **환경 변수** 버튼을 누르고 **Path**을 누르고 **편집**을 누른다.![image](https://github.com/strv103-0/youtube_dl/assets/112059527/83368f86-c7d9-484a-b450-81a6cdc72f3f)
-> 7. **새로 만들기**를 누른후 `C:\ffmpeg\bin`을 입력하고 저장하면 설정 끝이다.![image](https://github.com/strv103-0/youtube_dl/assets/112059527/83ca66aa-c5a2-4d7f-b054-d1d0531917c5)
--- main.py
+++ main.py
@@ -10,25 +10,23 @@ title ="""
 |     _______| |      |  |      ___|  |       |
 |    |_________|      |__|      \_____/       |
 |                                             |
-|                                    2.1.0    |
+|           Youtube mp3 Downloader  V: 2.2.1  |
 |_____________________________________________|
 """
 
-import subprocess
-import sys
 
 def install(package):
-    try:
-        # 파이썬 import 문을 사용하여 모듈 존재 확인
-        __import__(package)
-    except ImportError:
-        # 모듈이 없으면 pip를 사용하여 설치
-        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-# yt_dlp 라이브러리를 체크하고 필요시 설치합니다.
-install('yt_dlp')
-
-import yt_dlp
+        try:
+            # 파이썬 import 문을 사용하여 모듈 존재 확인
+            __import__(package)
+        except ImportError:
+            try:
+                # 모듈이 없으면 pip를 사용하여 설치
+                subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+            except subprocess.CalledProcessError:
+                # pip 설치 실패시 에러 메세지 출력
+                print(f"pip install {package} failed.")
+                sys.exit("pip install failed.")
 
 
 # 플레이리스트 다운로드 함수
@@ -36,7 +34,7 @@ def download_playlist(url):
     ydl_opts = {
         'format': 'bestaudio/best',                       # 영상화질 선택(화질을 선택하지 않으면 가장 좋은 화질로 다운로드)
         'outtmpl': 'downloads/%(title)s.%(ext)s',         # 다운로드 경로 설정
-        'ffmpeg_location': 'C:/ffmpeg/bin/ffmpeg.exe',    # ffmpeg.exe 위치 지정
+        'ffmpeg_location': './ffmpeg.exe',                # ffmpeg.exe 위치 지정
         'ignoreerrors': True,                             # 오류 발생시 건너뛰기
         'postprocessors': [{                              # 파일 변환
             'key': 'FFmpegExtractAudio',                  # 변환 모듈 선택
@@ -44,8 +42,53 @@ def download_playlist(url):
             }],
     }
     
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:               # 다운로드 시작
-        ydl.download([url])
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:               # 다운로드 시작
+            ydl.download([url])
+    except yt_dlp.utils.DownloadError:
+        print("다운로드에 실패하였습니다.")
+        sys.exit("Failed to download.")
+
+# 파이썬 기본 모듈
+try:
+    import subprocess
+    import sys
+    import zipfile
+    import os
+except ImportError:
+    print("파이썬 기본 모듈을 불러올 수 없습니다.")
+    sys.exit("Failed to import python basic module.")
+
+# 라이브러리를 체크하고 필요시 설치합니다.
+install('yt_dlp')
+
+# 파이썬 외부 모듈
+try:
+    import yt_dlp
+except ImportError:
+    print("파이썬 외부 모듈을 불러올 수 없습니다.")
+    sys.exit("Failed to import python external module.")
+
+
+# path
+ffmpeg_zip_path = "ffmpeg.zip"
+ffmpeg_exe_path = "ffmpeg.exe"
+
+
+if os.path.isfile(ffmpeg_zip_path) == True:
+    if os.path.isfile(ffmpeg_exe_path) == False:
+        try:
+            fantasy_zip = zipfile.ZipFile(ffmpeg_zip_path)
+            fantasy_zip.extract('ffmpeg.exe', './')
+            fantasy_zip.close()
+        except zipfile.BadZipFile:
+            print("ffmpeg.zip 파일이 손상되었습니다.")
+            sys.exit("ffmpeg.zip is broken.")
+    if os.path.isfile(ffmpeg_exe_path) == False:
+        print("ffmpeg.exe 파일이 존재하지 않습니다.")
+        sys.exit("ffmpeg.exe is not found.")
+    if os.path.isfile(ffmpeg_zip_path) == True:
+        os.remove(ffmpeg_zip_path)
 
 # title 출력
 print(title+"\n")
